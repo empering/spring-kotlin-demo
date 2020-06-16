fd48c07211a329e7228ca62a9065df9da4e52041
jda <jda@ibdata.co.kr>
2020-06-16 18:06:01 +0900

    h2 tcp 서버 추가 및 테스트 data

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (id, name, email, position) VALUES (1, '전대언', 'jda@ibdata.co.kr', 'C');
+INSERT INTO user (id, name, email, position) VALUES (2, '강대우', 'jda@ibdata.co.kr', 'D');
+INSERT INTO user (id, name, email, position) VALUES (3, '권시현', 'jda@ibdata.co.kr', 'D');
