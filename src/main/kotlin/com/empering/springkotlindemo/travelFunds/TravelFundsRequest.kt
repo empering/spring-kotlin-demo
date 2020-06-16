aff81601a6438eb83b70cf4229b03eaae7526af9
jda <jda@ibdata.co.kr>
2020-06-16 10:30:12 +0900

    entity class data class -> class

--- src/main/kotlin/com/empering/springkotlindemo/travelFunds/TravelFundsRequest.kt
+++ src/main/kotlin/com/empering/springkotlindemo/travelFunds/TravelFundsRequest.kt
@@ -7,23 +7,26 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
-data class TravelFundsRequest (
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        var id: Long? = null,
-        var travelBusiniss: String,
-        var travelArea: String,
-        var travelOrgan: String,
-        @Temporal(TemporalType.DATE)
-        @DateTimeFormat(pattern = "yyyy-MM-dd")
-        var startDate: Date? = null,
-        @Temporal(TemporalType.DATE)
-        @DateTimeFormat(pattern = "yyyy-MM-dd")
-        var endDate: Date? = null,
-        var days: Int,
-        var nights: Int,
-        var amount: Long = 0,
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name="user_id")
-        var user: User
-)
+class TravelFundsRequest() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+    var travelBusiniss: String? = ""
+    var travelArea: String? = ""
+    var travelOrgan: String? = ""
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    var startDate: Date? = null
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    var endDate: Date? = null
+    var days: Int? = 0
+    var nights: Int? = 0
+    var amount: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "user_id")
+    var user: User? = null
+}
--- src/main/kotlin/com/empering/springkotlindemo/user/User.kt
+++ src/main/kotlin/com/empering/springkotlindemo/user/User.kt
@@ -1,17 +1,31 @@
 package com.empering.springkotlindemo.user
 
 import com.empering.springkotlindemo.travelFunds.TravelFundsRequest
-import java.util.HashSet
+import java.util.*
 import javax.persistence.*
 
 @Entity
-data class User(
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        var id: Long? = null,
-        var name: String,
-        var email: String,
-        var position: String,
-        @OneToMany(mappedBy = "user")
-        var travelFundsRequests: MutableSet<TravelFundsRequest> = HashSet()
-)
+class User() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+    var name: String = ""
+    var email: String = ""
+    var position: String = ""
+
+    @OneToMany(mappedBy = "user")
+    var travelFundsRequests: MutableSet<TravelFundsRequest> = HashSet()
+
+    fun getTravelFundsRequests(): List<TravelFundsRequest> =
+            travelFundsRequests.sortedWith(compareBy { it.startDate })
+
+    fun getTravelFundsRequest(id: Long): TravelFundsRequest? {
+        for (travelFundsRequest in travelFundsRequests) {
+            if (travelFundsRequest.id == id) {
+                return travelFundsRequest
+            }
+        }
+
+        return null
+    }
+}
