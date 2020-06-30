02702a2e7a75e58f8316eab9adbe0e4d714d2ce7
jda <jda@ibdata.co.kr>
2020-06-30 17:33:41 +0900

    H2ServerConfiguration 수정
    prod모드가 아닌경우에만 tcp서버 실행

--- src/main/kotlin/com/empering/springkotlindemo/config/H2ServerConfiguration.kt
+++ src/main/kotlin/com/empering/springkotlindemo/config/H2ServerConfiguration.kt
@@ -5,10 +5,11 @@ import org.h2.tools.Server
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import javax.sql.DataSource
 
 @Configuration
-// @Profile("local")
+@Profile("!prod")
 class H2ServerConfiguration {
 
     @Bean
